4daa7b4e9a81dd12c1558466649a81d3dc51c942
Aladdin-2 <aladdin19.11.21@gmail.com>
2025-03-20 22:33:03 +0400

    Changes in ENTITIES. DTO, Services and Controllers added

--- src/main/java/com/team/updevic001/controllers/UserController.java
+++ src/main/java/com/team/updevic001/controllers/UserController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-public class UserControllerImpl {
+public class UserController {
 
     private final UserService userService;
     private final TestRepo testRepo;
