e67f0d335af50b2cfcae2997e11b6e4628cc2585
Aladdin-2 <aladdin19.11.21@gmail.com>
2025-04-04 20:15:30 +0400

    Authenticated User added

--- src/main/java/com/team/updevic001/dao/repositories/TeacherRepository.java
+++ src/main/java/com/team/updevic001/dao/repositories/TeacherRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public interface TeacherRepository extends JpaRepository<Teacher, String> {
 
-    Optional<Teacher> findTeacherByuser.getId()(String userId);
+    Optional<Teacher> findTeacherByUserId(String userId);
 
     @Modifying
     @Transactional
